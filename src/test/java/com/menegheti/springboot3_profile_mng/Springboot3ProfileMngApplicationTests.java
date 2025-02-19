8efc812089bfdc4843268d81b85729746d620922
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-02-19 11:44:44 -0300

    Implementação inicial

---
+++ src/test/java/com/menegheti/springboot3_profile_mng/Springboot3ProfileMngApplicationTests.java
@@ -0,0 +1,13 @@
+package com.menegheti.springboot3_profile_mng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot3ProfileMngApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
