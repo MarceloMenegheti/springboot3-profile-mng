e7893e4325681901ea952e384f76776dbcb62154
Marcelo Menegheti <marcelomenegheti18@gmail.com>
2025-02-19 12:41:13 -0300

    implementando a Entity - Person

---
+++ src/main/java/com/menegheti/springboot3_profile_mng/entity/Person.java
@@ -0,0 +1,93 @@
+package com.menegheti.springboot3_profile_mng.entity;
+
+import java.util.Objects;
+
+public class Person {
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String email;
+	
+	
+	public Person() {
+		
+	}
+
+	public Person(Long id, String firstName, String lastName, String userName, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.userName = userName;
+		this.email = email;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [id=" + id + ", firstName=" + firstName + ", userName=" + userName + ", email=" + email + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(email, firstName, id, lastName, userName);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return Objects.equals(email, other.email) && Objects.equals(firstName, other.firstName)
+				&& Objects.equals(id, other.id) && Objects.equals(lastName, other.lastName)
+				&& Objects.equals(userName, other.userName);
+	}
+	
+	
+  
+}
+
